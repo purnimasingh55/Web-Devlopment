c8c7d241b2d8dc26c5a42c23de52bb673daf60f1
purnimasingh55 <97280900+purnimasingh55@users.noreply.github.com>
2022-08-13 22:23:35 +0530

    Add files via upload

---
+++ Form Validation/index.js
@@ -0,0 +1,24 @@
+function validate(){
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var cpassword = document.getElementById('cpassword').value
+    
+
+    checkusername(username)
+    checkemail(email)
+    checkpassword(password)
+    checkpasswordmatch(password, cpassword)
+} 
+
+function checkusername(username){
+    if(username.length >7){
+        document.getElementById('username').classList.replace( 'error','success') 
+        
+    }
+    else{
+        document.getElementById('username').classList.add('error')
+
+    }
+
+}
