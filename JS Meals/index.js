6e353271fc56dfa3a9233f3b4f725bea00efd7b2
purnimasingh55 <97280900+purnimasingh55@users.noreply.github.com>
2023-11-10 09:42:13 +0530

    index.js

--- JS Meals/index.js
+++ JS Meals/index.js
@@ -1,19 +1,21 @@
-$.ajax ({
-    type : 'get',
-    url : 'https://www.themealdb.com/api/json/v1/1/filter.php?a=canadian',
-    success : function(response){
-        for(var i=0;i<response.meals.length;i++){
-            var newitem = `<div class = 'col-md-3 m-2'>
-            <p> ${response.meals[i].strMeal} </p>
-            <img src = ${response.meals[i].strMealThumb}/>
-            <p> ${response.meals[i].idMeal}</p>
-          </div>` 
+$.ajax({
+    type:'get',
+    url:'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian',
+    success:function(response){
+       for(var i=0;i<response.meals.length;i++){
 
-            $('#myiteams').append(newitem)
-        }
-        //console.log(responce)
+        var newitem=`<div class='col-md-3 m-2 shadow-lg p-3 mb-5 bg-white rounded text-center'>
+            <p>${response.meals[i].strMeal}</p>
+            <img src=${response.meals[i].strMealThumb} class='img-fluid'/>
+            <p>${response.meals[i].idMeal}</p>
+        </div>`
+           
+        $('#myitems').append(newitem)
+
+       }
     },
-    error : function(error){
+    error:function(error){
         console.log(error)
     }
+
 })
